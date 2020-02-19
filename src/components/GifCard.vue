3028071e9a795150104a6c9837095f9683896545
Helena Paixão <hp.helenapaixao@gmail.com>
2020-02-19 16:46:23 -0300

    add modal com informações do gif

--- src/components/GifCard.vue
+++ src/components/GifCard.vue
@@ -22,6 +22,29 @@
       >
         Favoritar
       </a>
+
+     <div>
+   <!-- Button trigger modal -->
+<button class="button is-primary modal-button" v-on:click="isShowModal = true">
+  Informações
+</button>
+
+<div class="modal" v-bind:class="{ 'is-active': isShowModal }">
+  <div class="modal-background" v-on:click="isShowModal = false"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">{{gifData.title}}</p>
+      <button class="delete" aria-label="close" v-on:click="isShowModal = false"></button>
+    </header>
+    <section class="modal-card-body">
+     <img :src="gifData.imageUrl"/>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button" v-on:click="isShowModal = false">Fechar</button>
+    </footer>
+  </div>
+</div>
+   </div>
     </footer>
   </div>
 </template>
@@ -41,10 +64,14 @@ export default {
   data() {
     return {
       wasCopied: false,
+      isShowModal: false
     };
   },
 
   methods: {
+    openModal() {
+
+    },
     share(url) {
       this.wasCopied = true;
       copyToClipboard(url);
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import SearchedGifsStore from './SearchedGifsStore';
 import TrendGifsStore from './TrendGifsStore';
+
 import Services from '@/Services';
 
 const { gifsApi } = Services();
@@ -9,6 +10,7 @@ const { gifsApi } = Services();
 const modules = {
   SearchedGifsStore: SearchedGifsStore({ gifsApi }),
   TrendGifsStore: TrendGifsStore({ gifsApi }),
+
 };
 
 Vue.use(Vuex);
