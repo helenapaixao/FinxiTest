48b262490782f01a6de84bc4c5ae2de0074141e2
Helena Paixão <hp.helenapaixao@gmail.com>
2020-02-19 14:13:24 -0300

    organizando as principais paginas do projeto

---
+++ src/pages/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Hero v-model="keyword"/>
+    <section v-if="keyword" class="section searched-gifs-container">
+      <h1 class="title is-4">Procurando o gif</h1>
+    </section>
+    <section>
+      <h1>Gifs que podem salvar o mundo</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {
+    Hero
+  },
+
+  async created() {
+
+  },
+
+  data() {
+
+  },
+  watch: {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import Home from './pages/Home.vue';
 
 Vue.use(VueRouter);
 
-const Routes = [
+const routes = [
   {
     path:'/',
     name:'home',
