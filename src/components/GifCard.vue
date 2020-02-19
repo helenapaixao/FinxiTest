b77c6869680985603a76eddfc9234a6d59dd05cb
Helena Paixão <hp.helenapaixao@gmail.com>
2020-02-19 17:19:02 -0300

    configurando style do gifcard

--- src/components/GifCard.vue
+++ src/components/GifCard.vue
@@ -1,68 +1,53 @@
 <template>
   <div class="card">
     <div class="card-image">
-      <img
-        class="image"
-        :src="gifData.downsampledUrl"
-        :alt="gifData.title"
-      />
+      <img class="image" :src="gifData.downsampledUrl" :alt="gifData.title" />
     </div>
     <footer class="card-footer">
-      <span
-        v-if="wasCopied"
-        class="card-footer-item copied-clipboard-text"
-      >
-        Gif favoritado!
+      <span v-if="wasCopied" class="card-footer-item copied-clipboard-text">
+        Gif salvo!
       </span>
 
-      <a
-        v-else
-        @click="share(gifData.url)"
-        class="card-footer-item share-button"
-      >
-        Favoritar
+      <a v-else @click="share(gifData.url)" class="card-footer-item share-button">
+        Salvar Gif
       </a>
 
-     <div>
-   <!-- Button trigger modal -->
-<button class="button is-primary modal-button" v-on:click="isShowModal = true">
-  Informações
-</button>
-
-<div class="modal" v-bind:class="{ 'is-active': isShowModal }">
-  <div class="modal-background" v-on:click="isShowModal = false"></div>
-  <div class="modal-card">
-    <header class="modal-card-head">
-      <p class="modal-card-title">{{gifData.title}}</p>
-      <button class="delete" aria-label="close" v-on:click="isShowModal = false"></button>
-    </header>
-    <section class="modal-card-body">
- <img
-        class="image"
-        :src="gifData.downsampledUrl"
-        :alt="gifData.title"
-      />
-    </section>
-    <footer class="modal-card-foot">
-      <button class="button" v-on:click="isShowModal = false">Fechar</button>
-    </footer>
-  </div>
-</div>
-   </div>
+      <div>
+        <div class="control">
+        <button class="button is-primary modal-button" v-on:click="isShowModal = true">
+          Detalhe
+        </button>
+        </div>
+        <div class="modal" v-bind:class="{ 'is-active': isShowModal }">
+          <div class="modal-background" v-on:click="isShowModal = false"></div>
+          <div class="modal-card">
+            <header class="modal-card-head">
+              <p class="modal-card-title">{{ gifData.title }}</p>
+              <button class="delete" aria-label="close" v-on:click="isShowModal = false"></button>
+            </header>
+            <section class="modal-card-body">
+              <img class="image" :src="gifData.downsampledUrl" :alt="gifData.title" />
+            </section>
+            <footer class="modal-card-foot">
+              <button class="button" v-on:click="isShowModal = false">Fechar</button>
+            </footer>
+          </div>
+        </div>
+      </div>
     </footer>
   </div>
 </template>
 
 <script>
-import copyToClipboard from '@/utils/copyToClipboard';
+import copyToClipboard from "@/utils/copyToClipboard";
 
 export default {
-  name: 'GifCard',
+  name: "GifCard",
 
   props: {
     gifData: {
-      type: Object,
-    },
+      type: Object
+    }
   },
 
   data() {
@@ -73,18 +58,17 @@ export default {
   },
 
   methods: {
-    openModal() {
-
-    },
+    openModal() {},
     share(url) {
       this.wasCopied = true;
       copyToClipboard(url);
 
-      setTimeout(() => { this.wasCopied = false; }, 1000);
-    },
-  },
+      setTimeout(() => {
+        this.wasCopied = false;
+      }, 1000);
+    }
+  }
 };
 </script>
 
-<style>
-</style>
+<style></style>
